48c7915a88c3e6e18604589e141e69cb0f24c83e
Konstantin Mukhin <kostik.mukhin.1995@gmail.com>
2023-01-16 20:36:48 +0300

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    semi: [2, 'always'],
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never' }
+    ],
+    'multiline-ternary': ['off'],
+    quotes: [
+      'error',
+      'single',
+      { allowTemplateLiterals: true, avoidEscape: true }
+    ],
+    'no-console': 'warn'
+  }
+};
