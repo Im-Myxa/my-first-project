bc848d3b41b443a93d7e6143cf2e9493645b7651
Konstantin Mukhin <kostik.mukhin.1995@gmail.com>
2023-03-28 20:24:11 +0400

    initial

--- server/app.js
+++ server/app.js
@@ -19,13 +19,13 @@ app.use("/api", router);
 
 const PORT = config.get("port") ?? 8080;
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use("/", express.static(path.join(__dirname, "client")));
-//   const indexPath = path.join(__dirname, "client", "index.html");
-//   app.get("*", (req, res) => {
-//     res.sendFile(indexPath);
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+  const indexPath = path.join(__dirname, "client", "index.html");
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 mongoose.syncIndexes("strictQuery", true);
 
 async function start() {
