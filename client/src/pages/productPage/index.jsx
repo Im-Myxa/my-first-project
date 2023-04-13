38de21f4dca8125115c26dda128b45410bc60ed9
Konstantin Mukhin <kostik.mukhin.1995@gmail.com>
2023-04-13 20:26:18 +0300

    function add product in basket for product page

--- client/src/pages/productPage/index.jsx
+++ client/src/pages/productPage/index.jsx
@@ -1,10 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from '../../services/axios';
+import { addProductInBasket } from '../../store/features/basket/basketSlice';
 
 const ProductPage = () => {
   const [product, setProduct] = useState(null);
   const params = useParams();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector(state => state.auth);
 
   const getProduct = useCallback(async () => {
     const { data } = await axios.get(`/products/${params.productId}`);
@@ -20,6 +25,21 @@ const ProductPage = () => {
       <div className='py-10 text-center text-xl text-white'>Загрузка...</div>
     );
   }
+
+  const handleAddProductInBasket = async () => {
+    try {
+      const addProduct = {
+        userId: user._id,
+        productId: product._id,
+        name: product.name,
+        price: product.price,
+        image: product.image
+      };
+      await dispatch(addProductInBasket(addProduct));
+    } catch (error) {
+      return error;
+    }
+  };
 
   return (
     <div className='mx-auto my-10 space-y-4 bg-white text-main lg:w-[1000px] md:w-[750px] sm:w-[380px]'>
@@ -43,6 +63,7 @@ const ProductPage = () => {
       </div>
       <button
         type='button'
+        onClick={handleAddProductInBasket}
         className='mx-auto w-[288px] rounded-lg border border-main p-2 hover:bg-main hover:text-white'
       >
         Добавить в корзину
